cbc263d7a364a844ed661a228063eeba8c18af68
Nathan Crincket <crincketnathan@gmail.com>
2024-10-29 19:44:45 +0100

    debut frontenf

---
+++ frontend/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" >
+    <title>iMangerMieux</title>
+</head>
+<body>
+    <header class="head">
+        <img src="imgs/logo-noir.svg" alt="Logo iMangerMieux" class="head-logo">
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="aliments.php">Aliments</a></li>
+                <li><a href="journal.php">Journal</a></li>
+            </ul>
+        </nav>
+        <div class="head-profil">
+            <img src="imgs/icone-utilisateur-noir.svg" alt="Icone Profil" class="icone-profil">
+            <a href="profil.php" class="link-profil">Se connecter</a>
+        </div>
+    </header>
+</body>
+</html>
