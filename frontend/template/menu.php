257b04391cc0ba6e544e960cbdd10786e361262a
Nathan Crincket <crincketnathan@gmail.com>
2024-10-31 10:46:37 +0100

    header-vf-1

---
+++ frontend/template/menu.php
@@ -0,0 +1,29 @@
+<?php
+    function renderMenuToHTML($currentPageId) {
+        // un tableau qui d\'efinit la structure du site
+        $mymenu = array(
+            // idPage titre
+            'accueil' => array('ACCUEIL'),
+            'aliments' => array('ALIMENTS'),
+            'journal' => array('JOURNAL')
+        );
+
+        echo "<nav class='menu'>
+            <ul>";
+
+        foreach ($mymenu as $pageId => $pageTitle) {
+            $title = $pageTitle[0];
+            if($pageId === $currentPageId){
+                $class = 'class="currentpage" ';
+            } 
+            else {
+                $class = ' ';
+            }
+            echo "<li><a href='index.php?page={$pageId}' $class>$title</a></li>";
+        }
+
+        // Fin de la navigation
+        echo "  </ul>
+            </nav>";
+    }
+?>
